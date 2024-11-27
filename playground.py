cb9417a671470a3611912758dac38ff8880737f9
Yifan_Wang <abihith.velumuri@gmail.com>
2024-11-27 17:28:17 -0500

    cosserat rod

--- playground.py
+++ playground.py
@@ -1,17 +1,44 @@
 import torch
+from torchmin import minimize
 
-a = torch.tensor([0,3])
-b = torch.tensor([1,4])
-print(torch.gt(a[:,None],b[None,:]))
-print(torch.nonzero(torch.tensor([[0,0],[1,0]])))
+# def rosen(x):
+#     return torch.sum(100*(x[..., 1:] - x[..., :-1]**2)**2 
+#                      + (1 - x[..., :-1])**2)
 
-for i in range(1,8):
+# # initial point
+# x0 = torch.tensor([1., 8.])
+
+# # Select from the following methods:
+# #  ['bfgs', 'l-bfgs', 'cg', 'newton-cg', 'newton-exact', 
+# #   'trust-ncg', 'trust-krylov', 'trust-exact', 'dogleg']
+
+# # BFGS
+# result = minimize(rosen, x0, method='bfgs')
+# print(result)
+
+# # Newton Conjugate Gradient
+# result = minimize(rosen, x0, method='newton-cg')
+# print(result)
+
+# # Newton Exact
+# result = minimize(rosen, x0, method='newton-exact')
+# print(result)
+
+# a = torch.tensor([0,3])
+# b = torch.tensor([1,4])
+# print(torch.gt(a[:,None],b[None,:]))
+# print(torch.nonzero(torch.tensor([[0,0],[1,0]])))
+
+# for i in range(1,8):
+#     print(i)
+
+# c = torch.tensor([0,1,2,3,4,5,6,7,8,9])
+# print(c[5:8])
+
+# m = torch.randn(6, 5, 4, 3)
+# p = torch.randn(4)
+# # print(p.expand_as(m).size())
+# print(p.expand(3,4))
+
+for i in range(11,-1,-1):
     print(i)
-
-c = torch.tensor([0,1,2,3,4,5,6,7,8,9])
-print(c[5:8])
-
-m = torch.randn(6, 5, 4, 3)
-p = torch.randn(4)
-# print(p.expand_as(m).size())
-print(p.expand(3,4))
--- splineCurve3D.py
+++ splineCurve3D.py
@@ -1,7 +1,12 @@
+import yaml
+import math
 import torch
+from torchmin import minimize
 import numpy as np
 from scipy.interpolate import BSpline
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from os.path import dirname, join as pjoin
+import scipy.io as sio
 
 def expSE3(Psi,device):
     # exponential map se3 to SE3, refer to Muller's review
@@ -50,6 +55,7 @@ class splineCurve3D:
         q = torch.reshape(sites[:-1,None] + self.h[:,None]*self.tq[None,:], (3*len(self.h),))
         V = torch.fliplr(torch.vander(self.tq-1/2)) # 3 x 3
         self.invV = torch.linalg.inv(V).to(self.device)
+        self.hd = torch.tensor(self.h,device=device)
         self.T = None
         self.Tq = None
         # self.T = torch.zeros((self.N,len(self.s),4,4), device=self.device)#.to(self.device)
@@ -59,17 +65,20 @@ class splineCurve3D:
         # collocation
         self.S = torch.zeros((self.n,len(sites)), device=device)
         self.Q = torch.zeros((self.n,(len(sites)-1)*3), device=device)
-        self.D = torch.zeros_like(self.S,device=device)
+        # self.D = torch.zeros_like(self.S,device=device)
         for i in range(self.n):
             b = BSpline.basis_element(t[i:i+k+2])
-            db = b.derivative(nu=1)
+            # db = b.derivative(nu=1) # does not work due to duplicated knots
             active_s = torch.logical_and(sites>=t[i], sites<=t[i+k+1])
             active_q = torch.logical_and(q>=t[i], q<=t[i+k+1])
             self.S[i,active_s] = torch.from_numpy(b(sites[active_s])).float().to(self.device)
             self.Q[i,active_q] = torch.from_numpy(b(q[active_q])).float().to(self.device)
-            self.D[i,active_s] = torch.from_numpy(db(sites[active_s])).float().to(self.device)
+            # self.D[i,active_s] = torch.from_numpy(db(sites[active_s])).float().to(self.device)
         self.S[-1,-1] = 1
         # self.uc = torch.permute(c,(1,2,0))@self.S[None,:,:] # N x 3 x len(sites)
+        D_mat = sio.loadmat('D.mat')
+        # print(D_mat['D'].dtype)
+        self.D = torch.from_numpy(D_mat['D']).float().to(device)
         self.uc = None
         self.t = self.t.to(device)
 
@@ -98,7 +107,7 @@ class splineCurve3D:
         
         return Psi
 
-    def integrate_SE3(self,quadrature_pose=False):
+    def integrate_SE3(self,c,quadrature_pose=False):
         # print('integrate_se3')
         # self.T = torch.zeros((self.N,len(self.s),4,4), device=self.device)#.to(self.device)
         # self.T[:,0,:,:] = torch.eye(4,device=self.device)[None,:,:]
@@ -107,7 +116,7 @@ class splineCurve3D:
         # self.T = torch.tile(torch.eye(4,device=self.device),(self.N,1,1,1))
         # self.dT = np.empty((self.n,self.N,3,len(self.s),4,4))
         h = self.h*self.L
-        uq = self.c @ self.Q[None,:,:] # N x 3 x vnk, strain at quadrature points
+        uq = c @ self.Q[None,:,:] # N x 3 x vnk, strain at quadrature points
         # Xq = torch.zeros((self.N,3,6,len(self.s)-1), device=self.device)
         # Psi = torch.zeros((self.N,6,len(self.s)-1), device=self.device)
         # expPsit = torch.zeros((self.N,4,4,len(self.s)-1), device=self.device)
@@ -137,13 +146,13 @@ class splineCurve3D:
                 # Psiq1 = (Xq(1,:)' + [Uc(:,k);0;0;1])/2*GV.hsp(k)*(1/2-sqrt(15)/10);
                 # Psiq2 = (Xq(2,:)' + Xq(1,:)')/2*GV.hsp(k)*sqrt(15)/10;
                 # Psiq3 = (Xq(3,:)' + [Uc(:,k+1);0;0;1])/2*GV.hsp(k)*(1/2-sqrt(15)/10);
-                Psiq = torch.cat(((Xq[:,1,:] + torch.cat((self.uc[:,:,k],v),dim=1))/2*self.tq[0]*h[k], # N x 6
-                                (Xq[:,2,:] + Xq[:,1,:])/2*(1/2-self.tq[0])*h[k],
-                                -(Xq[:,3,:] + torch.cat((self.uc[:,:,k+1],v),dim=1))/2*self.tq[0]*h[k]), dim=0)
-                expPsiq = expSE3(Psiq)
-                self.Tq[:,k*3,:,:] = self.T[:,k,:,:]*expPsiq[:self.N,:,:]
-                self.Tq[:,k*3+1,:,:] = self.Tq[:,k*3-2,:,:]*expPsiq[self.N:2*self.N,:,:]
-                self.Tq[:,k*3+2,:,:] = self.T[:,k+1,:,:]*expPsiq[2*self.N:,:,:]
+                Psiq = torch.cat(((Xq[:,0,:] + torch.cat((self.uc[:,:,k],v),dim=1))/2*self.tq[0]*h[k], # N x 6
+                                (Xq[:,1,:] + Xq[:,1,:])/2*(1/2-self.tq[0])*h[k],
+                                -(Xq[:,2,:] + torch.cat((self.uc[:,:,k+1],v),dim=1))/2*self.tq[0]*h[k]), dim=0)
+                expPsiq = expSE3(Psiq,self.device)
+                self.Tq[:,k*3,:,:] = self.T[:,k,:,:].clone()*expPsiq[:self.N,:,:]
+                self.Tq[:,k*3+1,:,:] = self.Tq[:,k*3-2,:,:].clone()*expPsiq[self.N:2*self.N,:,:]
+                self.Tq[:,k*3+2,:,:] = self.T[:,k+1,:,:].clone()*expPsiq[2*self.N:,:,:]
 
     def get_position(self, config=None, sites=np.linspace(0,1,19)):
         # config: N x (nx2), no torsion for now
@@ -155,7 +164,7 @@ class splineCurve3D:
             self.c = torch.cat((torch.permute(torch.reshape(config, (self.N, self.n, 2)),(0,2,1)),torch.zeros((self.N,1,self.n),device=self.device)), dim=1) # N x 3 x n
             self.uc = self.c @ self.S[None,:,:] # N x 3 x len(sites)
             # self.T = None
-            self.integrate_SE3()
+            self.integrate_SE3(self.c)
         # if self.T is None:
         #     self.integrate_SE3()
         p = torch.zeros((self.N, len(sites), 3), device=self.device)
@@ -185,50 +194,78 @@ def zero_force(p):
     return 0*p
 
 class TDCR(splineCurve3D):
-    def __init__(self, t, k, L, sites, device, fe=zero_force):
-        super().__init__(t, k, L, sites, device)
+    def __init__(self, spline_config, robot_config, device, fe=zero_force):
+        # spline properties
+        n = spline_config['n']
+        k = spline_config['k']
+        breaks = torch.linspace(0, 1, k+n+1-2*k)
+        t = torch.cat((torch.zeros(k), breaks, torch.ones(k)))# knots
+        sites = torch.cat((torch.tensor([0]), breaks[:-1] + (breaks[1:] - breaks[:-1])/2, torch.tensor([1])))
+        super().__init__(t, k, spline_config['L'], sites, device)
         self.fe = fe
-        # read config file
 
-    # def solve_Cosserat_model(self):
+        # robot properties
+        PI = torch.tensor(math.pi,device=device)
+        self.n_tendons = robot_config['num_tendons']
+        beta = torch.arange(self.n_tendons,device=device)*2*PI/self.n_tendons
+        self.r = torch.stack((torch.cos(beta),torch.sin(beta),torch.zeros_like(beta,device=device)),dim=1)*robot_config['tendon_offset'] # num x 3
+        zz = torch.zeros((self.n_tendons,1,1),device=device)
+        self.r_hat = torch.cat((torch.cat((zz,-self.r[:,2,None,None],self.r[:,1,None,None]),dim=2),
+                                torch.cat((self.r[:,2,None,None],zz,-self.r[:,0,None,None]),dim=2),
+                                torch.cat((-self.r[:,1,None,None],self.r[:,0,None,None],zz),dim=2)),dim=1)
+        self.qub = robot_config['q_ub']
+        self.qlb = robot_config['q_lb']
+        # area = PI*(robot_config['r']**2)
+        I = PI*(robot_config['r']**4)/4
+        J = 2*I
+        # GV.Kse = diag([shear_modulus*area, shear_modulus*area, E*area])
+        self.Kbt = torch.diag(torch.tensor([robot_config['E']*I, robot_config['E']*I, robot_config['shear_modulus']*J],device=device))
 
-        # function [T,dpdc,lambda,coeffs,res,exitflag,dedxi,dedq,dpdL,dnbdc,dnbbdL] = collocation_spline_TDK(qa,fe,Fe,Me,init_guess,GV)
+        # variables
+        self.tau = None
+
+
+    def solve_Cosserat_model(self,q,init_guess=None):
         # Implementation based on cubic B-spline
         # https://www.math.ucdavis.edu/~bremer/classes/fall2018/MAT128a/lecture15.pdf
+        # https://pytorch-minimize.readthedocs.io/en/latest/api/generated/torchmin.minimize.html
         # Nov 2024, Yifan Wang
         # Kirchhoff tendon-driven robot
+        # q: N x num_tendons/2
 
-        # % tau = zeros(GV.num_tendons,1);
-        # % if qa(1) >= 0, tau(1) = qa(1); else, tau(3) = -qa(1); end
-        # % if qa(2) >= 0, tau(2) = qa(2); else, tau(4) = -qa(2); end
-        # tau = [qa(1:2);-qa(1:2)]; % push-pull
+        self.N = q.shape[0]
+        # self.tau = torch.zeros((self.N,self.n_tendons/2))
+        self.tau = torch.cat((q,-q),dim=1) # push-pull
         # % hsp0 = GV.hsp; D0 = GV.D;
         # GV.hsp = GV.hsp*qa(3); % 1xn
         # GV.D = GV.D/qa(3);
         # GV.dUdotdc = GV.dUdotdc/qa(3);
-        # %     counter = 0;
-        # global T dT Tq dTq lambda;
-        # T = zeros(4,4,GV.n+1); T(:,:,1) = eye(4);
-        # dT = zeros(4,4,3*(GV.dof),GV.n+1);
-        # Tq = zeros(4,4,GV.n*3);
-        # dTq = zeros(4,4,3*(GV.dof),GV.n*3); % 4x4x3dofxvn
-        # % [res,dedxi] = collocation_error(init_guess); exitflag = true; xic = init_guess;
+        if init_guess is None:
+            init_guess = torch.zeros((self.N,3,self.n),device=self.device)
 
-        # [coeffs,res,exitflag,output,dedxi] = fsolve(@(coeffs) collocation_sp_error(coeffs), init_guess, GV.options_sp);
+        res = minimize(
+            self.collocation_error, 
+            init_guess, 
+            method='l-bfgs', 
+            options=dict(line_search='strong-wolfe'),
+            max_iter=1,
+            disp=2
+            )
+        self.c = res.x
 
-
-    def collocation_error(self):
+    def collocation_error(self,c):
         #  c: N x 3 x n spline coefficients stacked
+        self.uc = c @ self.S[None,:,:] # N x 3 x len(sites)
 
         # forward kinematics
-        self.integrate_SE3(quadrature_pose=True)
+        self.integrate_SE3(c,quadrature_pose=True)
 
         # Setup tendon linear system
         gx = torch.zeros((self.N,3,len(self.s)),device=self.device)
 
         # quadrature for integration of distributed external force
-        fe = self.fe(self.Tq[:,:,0:2,3]) # N x 3(n-1) x 3
-        intfe = torch.cat((self.L*self.h[None,:,None]*(5*(fe[:,0::3,:] + fe[:,2::3,:]) + 8*fe[:,1::3,:])/18,\
+        fe = self.fe(self.Tq[:,:,0:3,3]) # N x 3(n-1) x 3
+        intfe = torch.cat((self.L*self.hd[None,:,None]*(5*(fe[:,0::3,:] + fe[:,2::3,:]) + 8*fe[:,1::3,:])/18,\
                             torch.zeros((self.N,1,3),device=self.device)),dim=1)
         Intfe = torch.cumsum(intfe.flip(1),dim=1)
 
@@ -240,16 +277,16 @@ class TDCR(splineCurve3D):
         # #G = zeros(3,3)
         # H = torch.zeros_like(A,device=self.device)
         # nbt = torch.zeros_like(a,device=self.device)
-        e3 = torch.tensor([0,0,1],device=self.device)
+        e3 = torch.tensor([0.0,0.0,1.0],device=self.device).tile(self.N,1) # Nx3
         I3 = torch.eye(3,device=self.device)
-        meL = torch.zeros(self.N,3)
+        meL = torch.zeros(self.N,3,device=self.device)
 
         # zz = torch.zeros((self.N,1,1,len(self.s)))
         # u_hat = torch.cat((torch.cat((zz,-self.uc[:,2,None,:],self.uc[:,1,None,:]),dim=2),
         #                    torch.cat((self.uc[:,2,None,:],zz,-self.uc[:,0,None,:]),dim=2),
         #                    torch.cat((-self.uc[:,1,None,:],self.uc[:,0,None,:],zz),dim=2)),dim=1)
 
-        for i in range(len(self.s),-1,-1): # for each collocation point, backward
+        for i in range(len(self.s)-1,-1,-1): # for each collocation point, backward
             
             # if i == len(self.s):
             #     meL = R'@Me
@@ -261,14 +298,15 @@ class TDCR(splineCurve3D):
             nbt = torch.zeros_like(a,device=self.device)
 
             for j in range(self.n_tendons): # these are all "local" variables
-                pb_si = torch.cross(self.uc[:,:,i],self.r[j,:].expand(self.N,3),dim=1) + e3[None,:] # N x 3
+                pb_si = torch.cross(self.uc[:,:,i],self.r[j,:].expand(self.N,3),dim=1) + e3 # N x 3
                 pb_s_norm = torch.linalg.vector_norm(pb_si,dim=1)
-                Fb_j = -self.tau[:,j]*pb_si/pb_s_norm # N x 3
+                Fb_j = -self.tau[:,j,None]*pb_si/pb_s_norm[:,None] # N x 3
                 nbt -= Fb_j
 
-                A_j = -(pb_si[:,:,None]@pb_si[:,None,:] - torch.square(pb_s_norm[:,None,None])*I3[None,...]) * (self.tau[:,j]/torch.pow(pb_s_norm,3)) # N x 3 x 3
+                A_j = -(pb_si[:,:,None]@pb_si[:,None,:] - torch.square(pb_s_norm[:,None,None])*I3[None,...]) *\
+                      (self.tau[:,j]/torch.pow(pb_s_norm,3))[:,None,None] # N x 3 x 3
                 G_j = -A_j @ self.r_hat[j,:,:]
-                a_j = A_j @ torch.cross(self.uc[:,:,i],pb_si,dim=1)
+                a_j = torch.squeeze(A_j @ torch.cross(self.uc[:,:,i],pb_si,dim=1)[...,None],dim=-1)
 
                 a = a + a_j
                 b = b + torch.cross(self.r[None,j,:], a_j, dim=1)
@@ -277,25 +315,40 @@ class TDCR(splineCurve3D):
                 H = H + self.r_hat[j,:,:]@G_j
 
                 if i == len(self.s)-1: # boundary condition
-                    meL = meL + torch.cross(self.r[None,j,:], Fb_j, dim=1)
+                    meL += torch.cross(self.r[None,j,:], Fb_j, dim=1)
 
             K = H + self.Kbt[None,:,:] # Nx3x3
-            nb = -nbt + self.T[:,i,...].transpose(-2,-1)@Intfe[:,len(self.s)-i-1,:] # Nx3 not local
-            mb = self.Kbt[None,:,:]@self.uc[:,:,i] # Nx3
+            nb = -nbt + torch.squeeze(self.T[:,i,0:3,0:3].transpose(-2,-1)@Intfe[:,len(self.s)-i-1,:,None],dim=-1) # Nx3 not local
+            mb = torch.squeeze(self.Kbt[None,:,:]@self.uc[:,:,i,None],dim=-1) # Nx3
 
             # Calculate ODE terms
-            gx[:,:,i] = -torch.linalg.solve(K, torch.cross(self.uc[:,:,i],mb,dim=1) + torch.cross(e3.expand_as(nb),nb) + b) # Nx3
+            gx[:,:,i] = -torch.linalg.solve(K, torch.squeeze(torch.cross(self.uc[:,:,i],mb,dim=1) + torch.cross(e3,nb,dim=1) + b)) # Nx3
 
         # lambda = [mb;nb]
         
         # Assemble collocation and boundary errors
-        bL = torch.linalg.solve(self.Kbt[None,:,:],meL) # N x 3
+        bL = torch.linalg.solve(self.Kbt[None,:,:],meL[...,None]) # N x 3
+        print(meL)
+        print(self.Kbt)
 
-        E = torch.cat((self.c@self.D[None,:,:], self.uc[:,:,-1,None]), dim=2) - torch.cat((gx, bL[:,None,:]), dim=2) # N x 3 x len(s)+1
-        return torch.sum(torch.square(E)) # careful here
+        col_err = torch.cat((torch.div(c@self.D[None,:,:],self.L), self.uc[:,:,-1,None]), dim=2) - torch.cat((gx, bL), dim=2) # N x 3 x len(s)+1
+
+        return torch.sum(torch.square(col_err)) # careful here
 
 
-# def main():
+def main():
+    with open("config.yaml", 'r') as stream:
+        config = yaml.safe_load(stream)
+    # print(config)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    torch.autograd.set_detect_anomaly(True)
+    robot = TDCR(config['spline'], config['robot'], device)
+    q = torch.tensor([[20,0],[0,20]],device=device)
+    init_guess1 = torch.cat((torch.zeros((1,1,12),device=device),7.64*torch.ones((1,1,12),device=device),torch.zeros((1,1,12),device=device)),dim=1)
+    init_guess2 = torch.cat((-7.64*torch.ones((1,1,12),device=device),torch.zeros((1,1,12),device=device),torch.zeros((1,1,12),device=device)),dim=1)
+    init_guess = torch.cat((init_guess1,init_guess2),dim=0)
+    robot.solve_Cosserat_model(q,init_guess=init_guess)
 #     k = 3 # degree
 #     n = 12 # number of control points
 #     breaks = torch.linspace(0, 1, k+n+1-2*k)
@@ -325,15 +378,17 @@ class TDCR(splineCurve3D):
 #     # ax.grid(True)
 #     # plt.show()
 
-#     ax = plt.figure().add_subplot(projection='3d')
-#     for i in range(p.shape[0]):
-#         ax.plot(p[i,:,0].numpy(), p[i,:,1].numpy(), p[i,:,2].numpy())
-#     # i=1
-#     # ax.plot(p[i,:,0].numpy(), p[i,:,1].numpy(), p[i,:,2].numpy())
-#     ax.axis('equal')
-#     ax.set_xlabel('x')
-#     ax.set_ylabel('y')
-#     ax.set_zlabel('z')
-#     plt.show()
+    p = robot.get_position().detach().cpu() # N x n x 3
+    ax = plt.figure().add_subplot(projection='3d')
+    for i in range(p.shape[0]):
+        ax.plot(p[i,:,0].numpy(), p[i,:,1].numpy(), p[i,:,2].numpy())
+    # i=1
+    # ax.plot(p[i,:,0].numpy(), p[i,:,1].numpy(), p[i,:,2].numpy())
+    ax.axis('equal')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    plt.show()
 
-# main()
+if __name__ == "__main__":
+    main()
